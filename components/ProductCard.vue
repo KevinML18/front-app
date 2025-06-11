cb0ed20ac56fe53a926b4aef75878c268ff05c5b
KevinML18 <kevinmoraleslasso@gmail.com>
2025-06-11 15:22:51 +0200

    Responsive

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,55 +1,56 @@
 <template>
-  <div class="flex items-center bg-gray-900 rounded-xl p-5 shadow-sm space-x-4">
-    <div class="w-20 h-20 md:w-40 md:h-40 flex items-center justify-center overflow-hidden">
-      <img
-        :src="shop !== 'Fnac' ? image : 'https://1000marcas.net/wp-content/uploads/2021/06/Fnac-Logo-1985.jpg'"
-        alt="Producto"
-        class="object-contain w-full h-full"
-      />
-    </div>
-    <div class="flex-1">
-      <a
-        :href="url" 
-        :title="name"
-        class="md:text-xl text-sm font-semibold text-emerald-400 hover:text-emerald-600 active:text-emerald-700"
-        target="_blank"
-      >
-        {{ truncatedTitle }}
-      </a>
-      <p>{{ $t('shop') }}: {{ shop }}</p>
-      <p class="text-white text-lg">{{ $t('price') }}: {{ price }}€</p>
-    </div>
-    <div class="flex flex-col gap-3">
-      <button
-        v-if="favourite || route.name === 'account'"
-        class="bg-rose-600 hover:bg-rose-700 active:bg-rose-800 text-white font-medium px-4 py-2 rounded-lg cursor-pointer"
-        @click="deleteFavourite"
-      >
-        <DotLottieVue
-          v-if="loading"
-          src="animations/loading2.lottie"
-          autoplay
-          loop
-          class="w-27 h-6"
-        />
-        <span v-else class="text-sm">{{ $t('delete_favourite') }}</span>
-      </button>
-      <button
-        v-else
-        class="text-xs sm:text-base bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700 text-white font-medium px-4 py-2 rounded-lg cursor-pointer"
-        @click="addFavourite"
-      >
-        <DotLottieVue
-          v-if="loading"
-          src="animations/loading2.lottie"
-          autoplay
-          loop
-          class="w-27 h-6"
-        />
-        <span v-else>{{ $t('set_alert') }}</span>
-      </button>
-    </div>
+  <div class="flex flex-col sm:flex-row items-center bg-gray-900 rounded-xl p-3 sm:p-5 shadow-sm space-y-4 sm:space-y-0 sm:space-x-4">
+  <div class="w-24 h-24 sm:w-40 sm:h-40 flex items-center justify-center overflow-hidden">
+    <img
+      :src="shop !== 'Fnac' ? image : 'https://1000marcas.net/wp-content/uploads/2021/06/Fnac-Logo-1985.jpg'"
+      alt="Producto"
+      class="object-contain w-full h-full"
+    />
   </div>
+  <div class="flex-1 text-center sm:text-left space-y-1">
+    <a
+      :href="url" 
+      :title="name"
+      class="text-base sm:text-xl font-semibold text-emerald-400 hover:text-emerald-600 active:text-emerald-700 block"
+      target="_blank"
+    >
+      {{ truncatedTitle }}
+    </a>
+    <p class="text-white text-sm sm:text-base">{{ $t('shop') }}: {{ shop }}</p>
+    <p class="text-white text-lg">{{ $t('price') }}: {{ price }}€</p>
+  </div>
+  <div class="flex flex-row sm:flex-col gap-2 sm:gap-3 mt-2 sm:mt-0">
+    <button
+      v-if="favourite || route.name === 'account'"
+      class="bg-rose-600 hover:bg-rose-700 active:bg-rose-800 text-white text-xs sm:text-sm font-medium px-3 py-2 rounded-lg cursor-pointer w-full"
+      @click="deleteFavourite"
+    >
+      <DotLottieVue
+        v-if="loading"
+        src="animations/loading2.lottie"
+        autoplay
+        loop
+        class="w-27 h-6"
+      />
+      <span v-else>{{ $t('delete_favourite') }}</span>
+    </button>
+    <button
+      v-else
+      class="bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700 text-white text-xs sm:text-base font-medium px-3 py-2 rounded-lg cursor-pointer w-full"
+      @click="addFavourite"
+    >
+      <DotLottieVue
+        v-if="loading"
+        src="animations/loading2.lottie"
+        autoplay
+        loop
+        class="w-27 h-6"
+      />
+      <span v-else>{{ $t('set_alert') }}</span>
+    </button>
+  </div>
+</div>
+
 </template>
 
 <script setup>
@@ -57,7 +58,6 @@ import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
 import { useAuth } from '~/composables/auth/useAuth'
 import { eventBus } from '@/utils/eventBus'
 import { useRoute, useRouter } from 'vue-router'
-
 
 const { authUser } = useAuth()
 const route = useRoute()
--- middleware/auth.js
+++ middleware/auth.js
@@ -7,3 +7,4 @@ const { authUser } = useAuth()
     return navigateTo('/')
   }
 })
+
